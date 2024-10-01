c670c1d37a838a1f398e6858ae7fc1225f9e7833
Gabriel <correoclasegabri@gmail.com>
2024-10-01 22:44:11 +0200

    agaaaain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/GabriRDiaz.github.io/', // Set the base URL for your GitHub Pages deployment here
+  base: '/',
   define: {
     'process.env': process.env,
   },
